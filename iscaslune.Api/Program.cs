58a84b1fe80cd5803000440447fa98af4494d015
bruno <brunobentocaina@gmail.com>
2023-12-16 11:58:55 -0300

    fix

--- iscaslune.Api/Program.cs
+++ iscaslune.Api/Program.cs
@@ -18,6 +18,11 @@ internal class Program
         builder.Services.InjectApplication();
         builder.Services.InjectCors();
 
+        builder.Services.AddHttpsRedirection(options =>
+        {
+            options.HttpsPort = 443;
+        });
+
         var app = builder.Build();
 
         // Configure the HTTP request pipeline.
