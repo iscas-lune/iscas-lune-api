b2d284050f3f49d37b9fa6d284b2b83fb71b2ffc
Bruno <brunobentocaina@gmail.com>
2024-01-29 22:43:35 -0300

    fix: skip

--- iscas_lune_api/Infrastructure/Filtros/Model/FilterModel.cs
+++ iscas_lune_api/Infrastructure/Filtros/Model/FilterModel.cs
@@ -3,7 +3,7 @@
 public abstract class FilterModel
 {
     public string? Search { get; set; }
-    public int Skip {  get; set; }
+    public int Skip { get; set; } = 0;
     public int Take { get; set; } = 10;
     public string OrderBy { get; set; } = "DataCriacao";
     public bool Asc { get; set; } = false;
